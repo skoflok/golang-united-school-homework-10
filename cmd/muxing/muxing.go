579d243d54eb93230901c609f9833cfcc309b581
skoflok <skoflok@gmail.com>
2022-06-26 12:39:32 +0400

    handle func

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -20,6 +21,10 @@ main function reads host/port from env just for an example, flavor it following 
 // Start /** Starts the web server listener on given host and port.
 func Start(host string, port int) {
 	router := mux.NewRouter()
+	router.HandleFunc("/bad", getBad).Methods(http.MethodGet)
+	router.HandleFunc("/name/{param}", getName).Methods(http.MethodGet)
+	router.HandleFunc("/data", postData).Methods(http.MethodPost)
+	router.HandleFunc("/headers", postHeaders).Methods(http.MethodPost)
 
 	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
@@ -35,4 +40,34 @@ func main() {
 		port = 8081
 	}
 	Start(host, port)
+}
+
+func postData(w http.ResponseWriter, r *http.Request) {
+	if data, err := io.ReadAll(r.Body); err != nil {
+		w.WriteHeader(http.StatusOK)
+	} else {
+		fmt.Fprintf(w, string(data))
+	}
+}
+
+func postHeaders(w http.ResponseWriter, r *http.Request) {
+	a, err := strconv.Atoi(r.Header.Get("a"))
+	if err != nil {
+		w.WriteHeader(http.StatusOK)
+	}
+	b, err := strconv.Atoi(r.Header.Get("b"))
+	if err != nil {
+		w.WriteHeader(http.StatusOK)
+	}
+	sum := strconv.Itoa(a + b)
+	w.Header().Set("a+b", sum)
+}
+
+func getName(w http.ResponseWriter, r *http.Request) {
+	param := mux.Vars(r)["param"]
+	fmt.Fprintf(w, "Hello, "+param+"!\n")
+}
+
+func getBad(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
 }
